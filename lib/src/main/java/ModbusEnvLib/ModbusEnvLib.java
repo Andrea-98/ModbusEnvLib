304a9ce63d0de86dae317ad24f4d5d7187d00422
Andrea-98 <leccioli.andrea@gmail.com>
2022-10-27 12:30:22 +0200

    update

--- lib/src/main/java/ModbusEnvLib/ModbusEnvLib.java
+++ lib/src/main/java/ModbusEnvLib/ModbusEnvLib.java
@@ -54,18 +54,19 @@ public class ModbusEnvLib{
     public static void main(String[] args) {
 
             //// ----------TEST----------------//// 
-    /*        
-            ModbusClient modbusClient = new ModbusClient("192.168.0.1",502);
-		try
-		{
-			modbusClient.Connect();
-			System.out.println(modbusClient.ReadCoils(0, 1)[0]);
-			System.out.println(modbusClient.ReadHoldingRegisters(0, 1)[0]);
-			
+            
+        ModbusClient modbusClient = new ModbusClient("192.168.0.1",502);   // definire indirizzo ip e porta ( indirizzo ip è una stringa mentre la porta è un int)
+        try
+        {
+            modbusClient.Connect(); //per stabilire la connessione modbus tcp/ip
+            System.out.println(modbusClient.ReadCoils(0, 1)[0]);  //funzione per legere i coils (ripetere per ogni coil da leggere, il numero compreso tra parentesi quadre lasciare sempre a zero)
+            System.out.println(modbusClient.ReadHoldingRegisters(0, 1)[0]); //funzione per legere gli holding register (ripetere per ogni holding register da leggere, il numero compreso tra parentesi quadre lasciare sempre a zero)
+            modbusClient.WriteSingleCoil(1, true);  //per scrivere su singolo coil
+            modbusClient.WriteSingleRegister(2, 1234); //per scrivere su singolo registro
 		}
 		catch (Exception e)
 		{
 
-    }*/
+    }
 
 }}
